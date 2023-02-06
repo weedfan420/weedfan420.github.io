b634ccd34829d92e07c195f16ffb70dce55ad577
weedfan420 <rick.sanchez@weedfan.net>
2023-02-05 19:55:51 -0500

    added code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # weedfan420.github.io
-yeah...
+
+[add your description here]
+
+## License
+
+[add any licenses here]
+
+Solid State Theme From Solid State by HTML5 UP html5up.net | @ajlkn
---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$header = $('#header'),
+		$banner = $('#banner');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:	'(max-width: 1680px)',
+			large:	'(max-width: 1280px)',
+			medium:	'(max-width: 980px)',
+			small:	'(max-width: 736px)',
+			xsmall:	'(max-width: 480px)'
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Header.
+		if ($banner.length > 0
+		&&	$header.hasClass('alt')) {
+
+			$window.on('resize', function() { $window.trigger('scroll'); });
+
+			$banner.scrollex({
+				bottom:		$header.outerHeight(),
+				terminate:	function() { $header.removeClass('alt'); },
+				enter:		function() { $header.addClass('alt'); },
+				leave:		function() { $header.removeClass('alt'); }
+			});
+
+		}
+
+})(jQuery);
+
+function setOption(x) {
+	console.log(x);
+	document.getElementById("category").getElementsByTagName("option")[x].selected = true;
+}
+
+function setOption1() {
+	setOption(0);
+}
+function setOption2() {
+	setOption(1);
+}
+function setOption3() {
+	setOption(2);
+}
